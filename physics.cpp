e3af9ee7fdf9aabf71ec909b53d9aafb91c84e2f
Thomas Bement <thomasb@thomasbement.com>
2022-08-30 14:36:04 -0700

    added physics stuffs

--- physics.cpp
+++ physics.cpp
@@ -6,34 +6,54 @@
 
 // INCLUDES //
 #include <iostream>
+
 #include <string>
 
-// GLOBAL VARIABLES //
+#include <vector>
+using std::vector;
 
+#include "planets.h"
+
+// GLOBAL VARIABLES //
+const float dt = 1;
+const float G = 6.6743e-11; // [m.m.m/kg.s.s]
 
 // FUNCTIONS //
-
+void a_up(float dt, vector<planet> &planets) {
+    for (int i = 0; i < planets.size(); i++) {
+        for (int j = 0; j < i; j++) {
+            vec3 displacement = planets[i].p.sub(planets[j].p);
+            float distance = displacement.abs();
+            vec3 G_inverse_square_ij = displacement.scale(G / (distance * distance * distance));
+            planets[i].a = G_inverse_square_ij.scale(-planets[j].mass);
+            planets[j].a = G_inverse_square_ij.scale(planets[i].mass);
+        }
+    }
+    for (int i = 0; i < planets.size(); i++) {
+        planets[i].v_up(dt);
+        planets[i].p_up(dt);
+    }
+}
 
 // CLASSES //
-
-class vec3 {
-  public:
-    float x;
-    float y;
-    float z;
-    
-    vec3(float xx, float yy, float zz)
-      : x(xx), y(yy), z(zz) {}
-    
-    vec3 p(vec3 v) {
-        return vec3(x+v.x, y+v.y, z+v.z);
-    }
-};
 
 
 // MAIN //
 
-int main() 
-{
+int main() {
+    std::vector<planet> planets = {};
+
+    planet body_1(5.97219e24, vec3(0, 0, 0), vec3(0, 0, 0));
+    planet body_2(7.34767309e22, vec3(3.844e8, 0, 0), vec3(0, 1022, 0));
+
+    planets.push_back(body_1);
+    planets.push_back(body_2);
+    
+    while (true) {
+        std::cout << planets[0].p << "   " << planets[1].p << std::endl;
+        a_up(dt, planets);
+    }
+    
+    
     return 0;
 }
--- planets.h
+++ planets.h
@@ -5,12 +5,52 @@
 // ---------------------------------------- //
 
 // INCLUDES //
+#include <iostream>
+#include <cmath>
 
-class planet 
-{       
-    public:             
+class vec3 {
+  public:
+    float x;
+    float y;
+    float z;
+    
+    vec3(float xx, float yy, float zz)
+      : x(xx), y(yy), z(zz) {}
+    
+    vec3 plus(vec3 v) {
+        return vec3(x+v.x, y+v.y, z+v.z);
+    }
+
+    vec3 sub(vec3 v) {
+        return vec3(x-v.x, y-v.y, z-v.z);
+    }
+
+    vec3 scale(float a) {
+        return vec3(a*x, a*y, a*z);
+    }
+
+    float abs() {
+        return sqrt(x*x + y*y + z*z);
+    }
+};
+
+std::ostream &operator<<(std::ostream &os, vec3 const &m) { 
+    return os << "(" << m.x << "," << m.y << "," << m.z << ")";
+}
+
+class planet {       
+    public:            
         float mass;
-        float p[3] = {};
-        float v[3] = {};
-        float a[3] = {};
+        vec3 p;
+        vec3 v;
+        vec3 a;
+        planet(float mm, vec3 pp, vec3 vv) 
+            : mass(mm), p(pp), v(vv), a(0.0, 0.0, 0.0) {}
+    void p_up(float dt) {
+        this->p = this->p.plus(this->v.scale(dt));
+    }
+
+    void v_up(float dt) {
+        this->v = this->v.plus(this->a.scale(dt));
+    }
 };
