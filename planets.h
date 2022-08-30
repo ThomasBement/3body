ead066d50e7d2cabedbb5cbf499b658dd3291d2e
Thomas Bement <thomasb@thomasbement.com>
2022-08-30 12:22:58 -0700

    added local files

---
+++ planets.h
@@ -0,0 +1,16 @@
+// ---------------------------------------- //
+// planets [C++ Header File]
+// Written By: Thomas Bement
+// Created On: 2022-08-30
+// ---------------------------------------- //
+
+// INCLUDES //
+
+class planet 
+{       
+    public:             
+        float mass;
+        float p[3] = {};
+        float v[3] = {};
+        float a[3] = {};
+};
