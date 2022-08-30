be59764623565ec714777e7834dbab217a2aaf4d
Thomas Bement <thomasb@thomasbement.com>
2022-08-30 12:17:45 -0700

    added local files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 3body
+Techinicaly an n-body physics simulation with visualization via OpenGL.
---
+++ physics.cpp
@@ -0,0 +1,22 @@
+// ---------------------------------------- //
+// physics [C++ File]
+// Written By: Thomas Bement
+// Created On: 2022-08-29
+// ---------------------------------------- //
+
+// INCLUDES //
+#include <iostream>
+#include <string>
+
+// GLOBAL VARIABLES //
+
+
+// FUNCTIONS //
+
+
+// MAIN //
+
+int main() 
+{
+    return 0
+}
