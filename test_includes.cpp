44bd429fa98bde247cd07fb448fd7f8c22a492ee
phillip <phillipb163@gmail.com>
2022-08-31 14:41:32 -0700

    add a file to test that we can include all the required libraries

---
+++ test_includes.cpp
@@ -0,0 +1,30 @@
+// System Headers
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Math
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <math.h>
+
+// Standard Headers
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+using std::ifstream;
+#include <iostream>
+using std::cout; using std::endl;
+using std::string;
+#include <vector>
+using std::vector;
+
+
+int main() {
+    cout << "hello world!" << endl;
+    return 0;
+}
+
+
+
+
